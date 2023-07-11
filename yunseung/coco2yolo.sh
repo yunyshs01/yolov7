e895c9368702b9b47fb46654045b3320fee06f49
yunyshs01 <jeoung09@naver.com>
2023-07-11 11:31:51 +0900

    train code for aquarium dataset added

---
+++ yunseung/coco2yolo.sh
@@ -0,0 +1,3 @@
+python yunseung/coco2yolo.py -j aquarium_data/train/_annotations.coco.json -o  aquarium_data/train/
+python yunseung/coco2yolo.py -j aquarium_data/valid/_annotations.coco.json -o  aquarium_data/valid/
+python yunseung/coco2yolo.py -j aquarium_data/test/_annotations.coco.json -o  aquarium_data/test/
